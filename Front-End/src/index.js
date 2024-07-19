541f783ff625a7819a967fe529d6620bb079e35d
isthatananya <139475752+isthatananya@users.noreply.github.com>
2024-07-19 21:51:49 +0530

    Add files via upload
    hr_dashboard updated

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HrDashboard from "./hr_onboard";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+export default function MainApp() {
+  return (
+    <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MainApp />);
